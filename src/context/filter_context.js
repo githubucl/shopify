18c298204c887c8ecf72520990a6f3cc120f4709
chris Chen <59293007+githubucl@users.noreply.github.com>
2021-04-16 19:05:42 +1200

    added filtered products

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -14,7 +14,19 @@ import { useProductsContext } from './products_context'
 
 const initialState = {
   filtered_products: [],
-  all_products: []
+  all_products: [],
+  grid_view: true,
+  sort: 'price-lowest',
+  filters: {
+    text: '',
+    company: 'all',
+    category: 'all',
+    color: 'all',
+    min_price: 0,
+    max_price: 0,
+    price: 0,
+    shipping: false,
+  },
 }
 
 
@@ -27,11 +39,44 @@ export const FilterProvider = ({ children }) => {
   useEffect(() => {
     dispatch({ type: LOAD_PRODUCTS, payload: products })
   }, [products])
+
+  const setGridView = () => {
+    dispatch({ type: SET_GRIDVIEW })
+  }
+  const setListView = () => {
+    dispatch({ type: SET_LISTVIEW })
+  }
+
+  const updateSort = (e) => {
+    const value = e.target.value
+    dispatch({ type: UPDATE_SORT, payload: value })
+  }
+
+  const updateFilters = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+
+    dispatch({ type: UPDATE_FILTERS, payload: { name, value } })
+  }
+
+  const clearFilters = () => {
+
+  }
+
+  useEffect(() => {
+    dispatch({ type: FILTER_PRODUCTS })
+    dispatch({ type: SORT_PRODUCTS })
+  }, [products, state.sort, state.filters])
+
   return (
     <FilterContext.Provider value={
       {
         ...state,
-
+        setGridView,
+        setListView,
+        updateSort,
+        updateFilters,
+        clearFilters,
       }
     }>
       {children}
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -11,7 +11,57 @@ import {
 
 const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
-    return { ...state, all_products: [...action.payload], filtered_products: [...action.payload] }
+    const maxPrice = Math.max(...action.payload.map((product) => {
+      return product.price
+    }))
+
+    return {
+      ...state, all_products: [...action.payload], filtered_products: [...action.payload],
+      filters: { ...state.filters, max_price: maxPrice, price: maxPrice }
+    }
+  }
+  if (action.type === UPDATE_FILTERS) {
+    const { name, value } = action.payload
+
+    return { ...state, filters: { ...state.filters, [name]: value } }
+  }
+  if (action.type === FILTER_PRODUCTS) {
+    return { ...state }
+  }
+  if (action.type === SET_GRIDVIEW) {
+    return { ...state, grid_view: true }
+  }
+  if (action.type === SET_LISTVIEW) {
+    return { ...state, grid_view: false }
+  }
+  if (action.type === UPDATE_SORT) {
+    return { ...state, sort: action.payload }
+  }
+  if (action.type === SORT_PRODUCTS) {
+    const { sort, filtered_products } = state
+    let tempProduct = [...filtered_products]
+    if (sort === 'price-lowest') {
+      tempProduct = tempProduct.sort((a, b) => {
+        return a.price - b.price
+      })
+    }
+    if (sort === 'price-highest') {
+      tempProduct = tempProduct.sort((a, b) => {
+        return b.price - a.price
+      })
+    }
+    if (sort === 'name-a') {
+      tempProduct = tempProduct.sort((a, b) => {
+        return a.name.localeCompare(b.name)
+      })
+    }
+    if (sort === 'name-z') {
+      tempProduct = tempProduct.sort((a, b) => {
+        return b.name.localeCompare(a.name)
+      })
+    }
+
+    return { ...state, filtered_products: tempProduct }
   }
   throw new Error(`No Matching "${action.type}" - action type`)
 }
